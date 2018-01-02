a57492860d5ec9c04d659d9aa19685662c0b0012
Nolux <Atle.selbek@gmail.com>
2018-01-03 00:24:27 +0100

    added support for config.ini

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from appJar import gui
 from time import strftime
 import socket
+import configparser
 
-HOST = ''
-BUFSIZE = 20
-UDP_PORT = 53001
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+HOST = config['SETTINGS']['HOST']
+BUFSIZE = int(config['SETTINGS']['BUFSIZE'])
+UDP_PORT = int(config['SETTINGS']['UDP_PORT'])
+
+FONT_SIZE = int(config['SETTINGS']['FONT_SIZE'])
+FONT_COLOR = config['SETTINGS']['FONT_COLOR']
+BACKGROUND_COLOR = config['SETTINGS']['BACKGROUND_COLOR']
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((HOST, UDP_PORT))
@@ -19,6 +27,7 @@ def get_LTC():
 
 def quit(x):
     app.stop()
+    sock.close()
     print("Bye!")
 
 #refresh clock and timecode
@@ -31,6 +40,7 @@ def set_text():
 app = gui()
 
 app.bindKey("x", quit)
+app.bindKey("<Escape>", quit)
 
 app.setTitle("UDP to LTC display")
 app.setGeometry("fullscreen")
@@ -40,9 +50,9 @@ app.addLabel("time", "--:--")
 app.addLabel("ltc", "--:--:--:--")
 app.addLabel("filler2", " ")
 
-app.setFont(100)
-app.setBg("black")
-app.setFg("white")
+app.setFont(FONT_SIZE)
+app.setBg(BACKGROUND_COLOR)
+app.setFg(FONT_COLOR)
 
 app.thread(set_text())
 
