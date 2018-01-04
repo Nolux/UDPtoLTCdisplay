2e1d9703bcda489e4c5f61fadb66276a7732530d
Nolux <Atle.selbek@gmail.com>
2018-01-04 16:12:02 +0100

    added quit key as a variable in config Commented and cleaned up the code/config

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from time import strftime
 import socket
 import configparser
 
+#Load all variables from config file
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -14,10 +15,13 @@ FONT_SIZE = int(config['SETTINGS']['FONT_SIZE'])
 FONT_COLOR = config['SETTINGS']['FONT_COLOR']
 BACKGROUND_COLOR = config['SETTINGS']['BACKGROUND_COLOR']
 
+EXIT_KEY = config['SETTINGS']['EXIT_KEY']
+
+#Connect to server and bind port on local
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((HOST, UDP_PORT))
 
-#get timecode from UDP
+#Get timecode from UDP
 def get_LTC():
     data, addr = sock.recvfrom(BUFSIZE)
     if data is None:
@@ -25,12 +29,15 @@ def get_LTC():
     data_Decoded = data.decode("UTF-8")[0:11] #Making sure just to take "xx:xx:xx:xx"
     return data_Decoded
 
+
+#Exit app function called by keypress
 def quit(x):
     app.stop()
     sock.close()
     print("Bye!")
 
-#refresh clock and timecode
+
+#Refresh clock and timecode
 def set_text():
     app.setLabel("ltc", get_LTC())
     app.setLabel("time", strftime('%H:%M'))
@@ -39,12 +46,12 @@ def set_text():
 
 app = gui()
 
-app.bindKey("x", quit)
-app.bindKey("<Escape>", quit)
+app.bindKey(EXIT_KEY, quit)
 
 app.setTitle("UDP to LTC display")
 app.setGeometry("fullscreen")
 
+#Create all labels
 app.addLabel("filler", " ")
 app.addLabel("time", "--:--")
 app.addLabel("ltc", "--:--:--:--")
@@ -54,6 +61,7 @@ app.setFont(FONT_SIZE)
 app.setBg(BACKGROUND_COLOR)
 app.setFg(FONT_COLOR)
 
+#Start background loop
 app.thread(set_text())
 
 app.go()
